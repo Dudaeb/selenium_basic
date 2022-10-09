4a67a0d27c4d3c0b9cc5cb64c944ac73419aad1b
hp <dudaeb1996@gmail.com>
2022-10-10 01:53:48 +0600

    crome config update

--- webdriver_manager/crome_config.py
+++ webdriver_manager/crome_config.py
@@ -1,7 +1,17 @@
 from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.maximize_window()
-driver.get("http://www.iiuc.ac.bd")
+driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+time.sleep(15)
+driver.find_element(By.NAME, 'username').send_keys('Admin')
+time.sleep(15)
+driver.find_element(By.NAME, 'password').send_keys('admin123')
+time.sleep(15)
+driver.find_element(By.CSS_SELECTOR, '.orangehrm-login-button').click()
+time.sleep(15)
+driver.close()
