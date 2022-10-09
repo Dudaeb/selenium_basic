fd60c9e7053b4e4923e48e10f1e5cfb128445d4f
hp <dudaeb1996@gmail.com>
2022-10-10 01:27:57 +0600

    edge_config webdriver-manager update

---
+++ webdriver_manager/edge.config.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+driver.get("http://www.iiuc.ac.bd")
