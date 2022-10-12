1f1c268957eb47fab22e4c6ee67109610940752d
hp <dudaeb1996@gmail.com>
2022-10-12 19:30:23 +0600

    Upadate friefox-config

--- webdriver_manager/firefox_config.py
+++ webdriver_manager/firefox_config.py
@@ -1,6 +1,17 @@
+import time
+from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 
 driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
-driver.get('http://www.google.com')
+driver.maximize_window()
+driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+time.sleep(5)
+driver.find_element(By.NAME, 'username').send_keys('Admin')
+time.sleep(5)
+driver.find_element(By.NAME, 'password').send_keys('admin123')
+time.sleep(5)
+driver.find_element(By.CSS_SELECTOR, '.orangehrm-login-button').click()
+time.sleep(5)
+driver.close()
